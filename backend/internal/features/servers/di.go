d0f71f37191aaf0d49c8d1e3d89357cd77a250ad
anyagixx <anyagixx@users.noreply.github.com>
2025-12-28 01:38:10 +0300

    fix: убран databaseRepository из servers/di.go

--- backend/internal/features/servers/di.go
+++ backend/internal/features/servers/di.go
@@ -1,16 +1,15 @@
 package servers
 
 import (
-	"postgresus-backend/internal/features/databases"
 	"postgresus-backend/internal/util/encryption"
 	"postgresus-backend/internal/util/logger"
 )
 
 var serverRepository = &ServerRepository{}
-var databaseRepository = &databases.DatabaseRepository{}
+
 var serverService = &ServerService{
 	serverRepository,
-	databaseRepository,
+	
 	logger.GetLogger(),
 	encryption.GetFieldEncryptor(),
 }
